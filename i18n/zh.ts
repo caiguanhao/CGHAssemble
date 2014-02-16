cfcc52cc7e2cc16c2ab28b1eb2c7b3debf6f2cc8
Choi Goon-ho <caiguanhao@gmail.com>
2014-02-16 23:07:56 +0800

    update translations;

--- i18n/zh.ts
+++ i18n/zh.ts
@@ -3,154 +3,159 @@
 <context>
     <name>@default</name>
     <message>
-        <location filename="main.py" line="299"/>
+        <location filename="main.py" line="311"/>
         <source>Copy</source>
         <translation>复制</translation>
     </message>
     <message>
-        <location filename="main.py" line="192"/>
+        <location filename="main.py" line="200"/>
         <source>Source</source>
         <translation>源文件</translation>
     </message>
     <message>
-        <location filename="main.py" line="206"/>
+        <location filename="main.py" line="216"/>
         <source>Local</source>
         <translation>本地目录</translation>
     </message>
     <message>
-        <location filename="main.py" line="214"/>
+        <location filename="main.py" line="226"/>
         <source>Browse...</source>
         <translation>选择目录...</translation>
     </message>
     <message>
-        <location filename="main.py" line="100"/>
+        <location filename="main.py" line="104"/>
         <source>Up-to-date.</source>
         <translation>已更新。</translation>
     </message>
     <message>
-        <location filename="main.py" line="501"/>
+        <location filename="main.py" line="529"/>
         <source>CGHAssemble</source>
         <translation>CGHAssemble</translation>
     </message>
     <message>
-        <location filename="main.py" line="224"/>
+        <location filename="main.py" line="236"/>
         <source>Ready.</source>
         <translation>准备就绪。</translation>
     </message>
     <message>
-        <location filename="main.py" line="354"/>
+        <location filename="main.py" line="366"/>
         <source>Download/Update</source>
         <translation>下载/更新</translation>
     </message>
     <message>
-        <location filename="main.py" line="371"/>
+        <location filename="main.py" line="383"/>
         <source>Install</source>
         <translation>安装依赖包</translation>
     </message>
     <message>
-        <location filename="main.py" line="405"/>
+        <location filename="main.py" line="417"/>
         <source>Preview</source>
         <translation>预览网站</translation>
     </message>
     <message>
-        <location filename="main.py" line="476"/>
+        <location filename="main.py" line="488"/>
         <source>Assemble</source>
         <translation>编制网站</translation>
     </message>
     <message>
-        <location filename="main.py" line="298"/>
+        <location filename="main.py" line="310"/>
         <source>Copied!</source>
         <translation>已复制!</translation>
     </message>
     <message>
-        <location filename="main.py" line="302"/>
+        <location filename="main.py" line="314"/>
         <source>Select Folder</source>
         <translation>选择文件夹</translation>
     </message>
     <message>
-        <location filename="main.py" line="308"/>
+        <location filename="main.py" line="320"/>
         <source>Please select a folder which path contains no unicode characters.</source>
         <translation>请选择一个路径中没有中文字符的文件夹。</translation>
     </message>
     <message>
-        <location filename="main.py" line="349"/>
+        <location filename="main.py" line="361"/>
         <source>Connecting...</source>
         <translation>连接中...</translation>
     </message>
     <message>
-        <location filename="main.py" line="472"/>
+        <location filename="main.py" line="484"/>
         <source>Processing...</source>
         <translation>处理中...</translation>
     </message>
     <message>
-        <location filename="main.py" line="374"/>
+        <location filename="main.py" line="386"/>
         <source>All packages have been successfully installed.</source>
         <translation>已成功安装所有依赖包。</translation>
     </message>
     <message>
-        <location filename="main.py" line="401"/>
+        <location filename="main.py" line="413"/>
         <source>Close</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="main.py" line="413"/>
+        <location filename="main.py" line="425"/>
         <source>Preview is now closed.</source>
         <translation>已关闭预览。</translation>
     </message>
     <message>
-        <location filename="main.py" line="467"/>
+        <location filename="main.py" line="479"/>
         <source>Grunt is not installed in node_modules directory. Please click Install button first.</source>
         <translation>还未安装Grunt，请先按安装依赖包按钮。</translation>
     </message>
     <message>
-        <location filename="main.py" line="491"/>
+        <location filename="main.py" line="547"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="main.py" line="258"/>
+        <location filename="main.py" line="270"/>
         <source>lang-zh</source>
         <translation>lang-en</translation>
     </message>
     <message>
-        <location filename="main.py" line="282"/>
+        <location filename="main.py" line="294"/>
         <source>Changing the language will break current task. Please try again once current task completes.</source>
         <translation>更换语言会中断当前任务，请等任务结束后再试。</translation>
     </message>
     <message>
-        <location filename="main.py" line="258"/>
+        <location filename="main.py" line="270"/>
         <source>&#xe4;&#xb8;&#xad;&#xe6;&#x96;&#x87;</source>
         <translation>English</translation>
     </message>
     <message>
-        <location filename="main.py" line="452"/>
+        <location filename="main.py" line="464"/>
         <source>NPM is missing. You may need to re-install this software.</source>
         <translation>找不到NPM，请重新安装本软件。</translation>
     </message>
     <message>
-        <location filename="main.py" line="458"/>
+        <location filename="main.py" line="470"/>
         <source>Grunt is missing. You may need to re-install this software.</source>
         <translation>找不到Grunt，请重新安装本软件。</translation>
     </message>
     <message>
-        <location filename="main.py" line="440"/>
+        <location filename="main.py" line="452"/>
         <source>Node.js executable file is missing. You may need to re-install this software.</source>
         <translation>找不到Node.js执行文件，请重新安装本软件。</translation>
     </message>
     <message>
-        <location filename="main.py" line="449"/>
+        <location filename="main.py" line="461"/>
         <source>Node.js executable file is corrupted. You may need to re-install this software.</source>
         <translation>Node.js执行文件已损毁，请重新安装本软件。</translation>
     </message>
     <message>
-        <location filename="main.py" line="461"/>
+        <location filename="main.py" line="473"/>
         <source>The package.json file is not found in local directory. Please update your repository.</source>
         <translation>本地目录中找不到package.json文件，请更新源文件库。</translation>
     </message>
     <message>
-        <location filename="main.py" line="464"/>
+        <location filename="main.py" line="476"/>
         <source>The Gruntfile.js file is not found in local directory. Please update your repository.</source>
         <translation>本地目录中找不到Gruntfile.js文件，请更新源文件库。</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="547"/>
+        <source>CGHAssemble is already running.</source>
+        <translation>CGHAssemble程序已在运行。</translation>
     </message>
 </context>
 </TS>
